39f232daf92b3f60c5feaf2a53146d096ee55446
DamnFilthy <aleksandr-shiryaev-1993@mail.ru>
2020-12-14 15:52:23 +0300

    updated

--- homework_l2.py
+++ homework_l2.py
@@ -1,24 +1,31 @@
 import requests
 import json
 import os
-from pprint import pprint
 import hashlib
 
 
+# Задание №1
 # Класс итератора, который возвращает список стран
 class Downloader:
     def __init__(self, file_name):
-        self.file_name = file_name
+        self.file_name = open(file_name)
+        self.data = json.loads(self.file_name.read())
+        self.counter = -1
+        self.country_names = []
+        for i, name in enumerate(self.data):
+            self.country_names.append(self.data[i]['name']['common'])
 
-    def iterator(self):
-        with open(self.file_name) as file:
-            country_names = []
-            data = json.loads(file.read())
-        for i, name in enumerate(data):
-            country_names.append(data[i]['name']['common'])
-        return country_names
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.counter += 1
+        if self.counter >= len(self.country_names):
+            raise StopIteration
+        return self.country_names[self.counter]
 
 
+# Задание №2
 # генератор, который принимает путь к файлу. При каждой итерации возвращает md5 хеш каждой строки файла.
 def hash_generate(my_path):
     files = (os.listdir(my_path))
@@ -33,17 +40,18 @@ def hash_generate(my_path):
 
 
 if __name__ == '__main__':
-
+    # Задание №1
     # Используем итератор
-    my_file = Downloader('countries.json').iterator()
     with open('wiki_country.txt', 'w', encoding='UTF-8') as f:
+        data = Downloader('countries.json')
         link = 'wikipedia.org/wiki/'
         result = []
-        for country in my_file:
+        for country in data:
             new_link = link + country + '\n'
             result = country + ' : ' + new_link
             f.write(result)
 
+    # Задание №2
     # Используем генератор
     for country in hash_generate('C:\\Users\\User\\Desktop\\my_files'):
         print(country)
